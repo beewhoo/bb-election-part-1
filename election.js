580d55d869bb50306f3efd5640909104e65acfc2
beewhoo <bibeksinghtthapa@gmail.com>
2018-07-18 16:51:33 -0400

    finished ajax call and listed the candidates & votes

--- election.js
+++ election.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var showDiv = document.querySelector('.show')
+  var ul = document.createElement('ul');
+
 
   // Imagination!
-
+  $.ajax({
+    url:'https://bb-election-api.herokuapp.com/',
+    method: 'GET'
+  }).done(function(responseData) {
+    for (var i = 0; i < responseData.candidates.length; i++) {
+      console.log(responseData.candidates);
+      var li = document.createElement('li');
+      li.innerHTML = responseData.candidates[i].name + " - " + " votes: " + responseData.candidates[i].votes;
+      ul.append(li);
+      showDiv.append(ul);
+    }
+  })
 });
